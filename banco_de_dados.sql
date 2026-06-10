b98d83094e972af3768cfd560cfc728769b82a02
diogosilvasenai <diogo.f.silva@aluno.senai.br>
2026-06-10 10:09:10 -0300

    Update banco_de_dados.sql

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -1,13 +1,9 @@
-CREATE DATABASE IF NOT EXISTS velocimetro_iot
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
-USE velocimetro_iot;
-
-CREATE TABLE IF NOT EXISTS leituras (
-    id            INT UNSIGNED     NOT NULL AUTO_INCREMENT,
-    velocidade    DECIMAL(6, 2)    NOT NULL COMMENT 'Velocidade em km/h',
-    rpm           DECIMAL(8, 2)    NOT NULL COMMENT 'Rotações por minuto',
-    registrado_em TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Data/hora do registro',
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- criar o banco de dados
+create database velocimetro_iot;
+-- selecionar o banco de dados
+use velocimetro_iot;
+-- criar a tabela
+create table dados_velocidade (
+    kph float,
+    rpm int
+);
